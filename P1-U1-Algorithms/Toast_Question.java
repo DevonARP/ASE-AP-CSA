afabb34d4667e266ccc47860ddd26621bd13d3d4
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-24 21:52:52 -0400

    Create Toast_Question.java

---
+++ P1-U1-Algorithms/Toast_Question.java
@@ -0,0 +1,16 @@
+public class MyClass {
+  public static void main(String args[]) {
+      Toast x = new Toast();
+      x.makeToast();
+  }
+}
+
+
+class Toast {
+    public static void makeToast() {
+        // Print the three steps of the toast algorithm here.
+        // Each step should appear on its own line.
+    }
+}
+
+
