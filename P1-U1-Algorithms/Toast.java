40d129745dd3c495866798896769c7091491065d
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-24 21:51:20 -0400

    Create Toast.java

---
+++ P1-U1-Algorithms/Toast.java
@@ -0,0 +1,15 @@
+public class MyClass {
+  public static void main(String args[]) {
+      Toast x = new Toast();
+      x.makeToast();
+  }
+}
+
+
+class Toast {
+    public static void makeToast() {
+        System.out.println("Get bread");
+        System.out.println("Toast it");
+        System.out.println("Butter it");
+    }
+}
