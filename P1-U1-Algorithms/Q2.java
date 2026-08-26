917bc837c0de9e54fca2ccfccf632ace98091c77
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-26 00:14:00 -0400

    Create Q2.java

---
+++ P1-U1-Algorithms/Q2.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int result = castDoubleToInt(3.5);
+        
+        System.out.println("Integer: " + result);
+    }
+    
+    // Static function to add two numbers
+    public static int castDoubleToInt(double value) {
+        
+        //Write code here
+        
+        return 0; //Edit here
+    }
+}
