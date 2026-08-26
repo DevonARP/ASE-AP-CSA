f1d1c6f6543ecf1083d6435eb1dff96976729ba9
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-26 00:07:09 -0400

    Update Q1.java

--- P1-U1-Algorithms/Q1.java
+++ P1-U1-Algorithms/Q1.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Write code here
         
         System.out.println("The sum is: " + result);
     }
 
   
     public static int addNumbers(int a, int b) {
-        return 0;
+        return a+b;
     }
 }
