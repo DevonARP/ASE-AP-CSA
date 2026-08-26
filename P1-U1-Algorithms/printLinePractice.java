3a24ca72dc60b1d0f1405b9b36e554c5265b4823
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-25 22:39:21 -0400

    Create printLinePractice.java

---
+++ P1-U1-Algorithms/printLinePractice.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String args[]) {
+    
+    int count = 0;
+    double temperature = -15.15;
+    boolean isRaining = false;
+    
+    //Print them out
+    System.out.print(count);
+    System.out.print(temperature);
+    System.out.print(isRaining);
+    
+    System.out.println();
+    System.out.println(count);
+    System.out.println(temperature);
+    System.out.println(isRaining);
+
+  }
+}
