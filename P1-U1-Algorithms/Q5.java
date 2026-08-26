fb4a007520e0a5b9807cbdcfd6d8e53a2ee4bdc8
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-26 00:30:32 -0400

    Update Q5.java

--- P1-U1-Algorithms/Q5.java
+++ P1-U1-Algorithms/Q5.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         
-        double result; 
+        double result; //Write here
         
         System.out.println("It is: " + result);
     }
