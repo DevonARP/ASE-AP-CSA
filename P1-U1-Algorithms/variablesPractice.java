73714af721c59fd78823d787ba485a05f557ce48
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-25 22:32:34 -0400

    Create variablesPractice.java

---
+++ P1-U1-Algorithms/variablesPractice.java
@@ -0,0 +1,23 @@
+public class MyClass {
+  public static void main(String args[]) {
+     
+    //Declare and initialize variables
+    int count = 0;
+    double temperature = -15.15;
+    boolean isRaining = false;
+    
+    //Print them out
+    System.out.println(count);
+    System.out.println(temperature);
+    System.out.println(isRaining);
+    
+    //Constants
+    final int MAX_STUDENTS = 30;
+    System.out.println(MAX_STUDENTS);
+    
+    
+    //Update a variable
+    count = count + 1;
+    System.out.println(count);
+  }
+}
