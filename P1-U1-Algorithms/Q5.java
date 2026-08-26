be2971ac0989276f5b4d2a76fca0c8ab63da3b97
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-26 00:30:16 -0400

    Create Q5.java

---
+++ P1-U1-Algorithms/Q5.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        double result; 
+        
+        System.out.println("It is: " + result);
+    }
+    
+    public static double multiplyIntAndDouble(int a, double b) {
+        
+        //Write code here
+        
+        return 0; //Edit here
+    }
+}
