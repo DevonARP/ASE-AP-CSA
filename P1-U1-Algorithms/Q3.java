2831449a351cd80b1adc0f787e5d853c084da21a
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-26 00:23:37 -0400

    Create Q3.java

---
+++ P1-U1-Algorithms/Q3.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        boolean result; //Edit here
+        
+        System.out.println("It is: " + result);
+    }
+    
+    public static boolean isTrue(boolean value) {
+        
+        //Write code here
+        
+        return 0; //Edit here
+    }
+}
