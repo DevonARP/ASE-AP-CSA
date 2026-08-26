5930f6a1ba6c2b11e55c83d4f9f5d7f88a108b7a
Anil R. Poonai <50726395+DevonARP@users.noreply.github.com>
2026-08-26 00:28:12 -0400

    Create Q4.java

---
+++ P1-U1-Algorithms/Q4.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int result; //Edit here
+        
+        System.out.println("It is: " + result);
+    }
+    
+    public static int maxValueMinusOne() {
+        
+        //Write code here
+        
+        return 0; //Edit here
+    }
+}
